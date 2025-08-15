acb9e00c57d69adebe4f9f0cb3387da9c9ac3118
e.yankovskaya <elena.parokhina@gmail.com>
2025-08-15 10:29:08 +0200

    update countOccurrences()

--- src/main/java/ru/pro/CounterUtil.java
+++ src/main/java/ru/pro/CounterUtil.java
@@ -2,18 +2,18 @@ package ru.pro;
 
 import lombok.experimental.UtilityClass;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 @UtilityClass
 public class CounterUtil {
     public <T> Map<T, Integer> countOccurrences(T[] array) {
         Map<T, Integer> occurrences = new HashMap<>();
-        if (array != null) {
-            for (T element : array) {
-                occurrences.put(element, occurrences.getOrDefault(element, 0) + 1);
-            }
-        }
+        Optional.ofNullable(array)
+                .ifPresent(arr -> Arrays.stream(arr)
+                        .forEach(element -> occurrences.merge(element, 1, Integer::sum)));
         return occurrences;
     }
 }
