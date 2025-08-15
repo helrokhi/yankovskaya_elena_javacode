f71f04419824695b7a853ffc5887389fbfc19142
e.yankovskaya <elena.parokhina@gmail.com>
2025-08-15 10:31:19 +0200

    refactoring

--- src/main/java/ru/pro/CounterUtil.java
+++ src/main/java/ru/pro/CounterUtil.java
@@ -2,17 +2,18 @@ package ru.pro;
 
 import lombok.experimental.UtilityClass;
 
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Optional;
+
+import static java.util.Arrays.stream;
+import static java.util.Optional.ofNullable;
 
 @UtilityClass
 public class CounterUtil {
     public <T> Map<T, Integer> countOccurrences(T[] array) {
         Map<T, Integer> occurrences = new HashMap<>();
-        Optional.ofNullable(array)
-                .ifPresent(arr -> Arrays.stream(arr)
+        ofNullable(array)
+                .ifPresent(arr -> stream(arr)
                         .forEach(element -> occurrences.merge(element, 1, Integer::sum)));
         return occurrences;
     }
