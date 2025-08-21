10289759ac96a6205626a943ea9b3d554078d146
elena_iankovskaia <elena.parokhina@gmail.com>
2025-08-21 11:08:28 +0200

    start

---
+++ src/main/java/ru/pro/StreamCollectorsExample.java
@@ -0,0 +1,15 @@
+package ru.pro;
+
+import java.util.List;
+
+public class StreamCollectorsExample {
+    public static void main(String[] args) {
+        List<Order> orders = List.of(
+                new Order("Laptop", 1200.0),
+                new Order("Smartphone", 800.0),
+                new Order("Laptop", 1500.0),
+                new Order("Tablet", 500.0),
+                new Order("Smartphone", 900.0)
+        );
+    }
+}
