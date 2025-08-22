8a266ddda9d44eb95b8f2c099a3fa0a4b61ec955
elena_iankovskaia <elena.parokhina@gmail.com>
2025-08-22 06:56:57 +0200

    update StreamCollectorsExample

--- src/main/java/ru/pro/Order.java
+++ src/main/java/ru/pro/Order.java
@@ -1,19 +1,11 @@
 package ru.pro;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
 public class Order {
     private String product;
     private double cost;
-
-    public Order(String product, double cost) {
-        this.product = product;
-        this.cost = cost;
-    }
-
-    public String getProduct() {
-        return product;
-    }
-
-    public double getCost() {
-        return cost;
-    }
 }
