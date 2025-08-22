0e163ac4823fd6511cf5f42107e17dd0820e1d88
elena_iankovskaia <elena.parokhina@gmail.com>
2025-08-22 07:08:31 +0200

    start

---
+++ src/main/java/ru/pro/ParallelStreamCollectMapAdvancedExample.java
@@ -0,0 +1,16 @@
+package ru.pro;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+public class ParallelStreamCollectMapAdvancedExample {
+    public static void main(String[] args) {
+        List<Student> students = Arrays.asList(
+                new Student("Student1", Map.of("Math", 90, "Physics", 85)),
+                new Student("Student2", Map.of("Math", 95, "Physics", 88)),
+                new Student("Student3", Map.of("Math", 88, "Chemistry", 92)),
+                new Student("Student4", Map.of("Physics", 78, "Chemistry", 85))
+        );
+    }
+}
