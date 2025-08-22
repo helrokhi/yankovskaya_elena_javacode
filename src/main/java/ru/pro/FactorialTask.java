520788f23ea55a6c20929e2ef0189023ce41da78
elena_iankovskaia <elena.parokhina@gmail.com>
2025-08-22 08:05:15 +0200

    update FactorialTask

--- src/main/java/ru/pro/FactorialTask.java
+++ src/main/java/ru/pro/FactorialTask.java
@@ -1,5 +1,19 @@
 package ru.pro;
 
-public class FactorialTask {
+import lombok.AllArgsConstructor;
 
+import java.util.concurrent.RecursiveTask;
+
+@AllArgsConstructor
+public class FactorialTask extends RecursiveTask<Integer> {
+    private final int n;
+
+    @Override
+    protected Integer compute() {
+        if (n <= 1) return 1;
+        FactorialTask subTask = new FactorialTask(n - 1);
+        subTask.fork();
+        Integer subResult = subTask.join();
+        return n * subResult;
+    }
 }
